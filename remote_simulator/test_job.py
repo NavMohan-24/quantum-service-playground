783b80083f3094be4980eeff72b27d8efaf79729
Navaneeth Mohan <navaneethkrishnan24@gmail.com>
2025-11-28 07:13:28 +0530

    test job

---
+++ remote_simulator/test_job.py
@@ -0,0 +1,17 @@
+from qiskit import QuantumCircuit
+from remote_aer_backend import RemoteAerBackend
+
+# constructing circuit
+qc = QuantumCircuit(2,2)
+qc.h(0)
+qc.cx(0,1)
+qc.barrier()
+qc.measure_all()
+
+# executing it in remote-simulator
+
+backend = RemoteAerBackend()
+job = backend.run(qc, shots=4096)
+result = job.result()
+
+print("Backend results:", result[0].data.meas.get_counts())
