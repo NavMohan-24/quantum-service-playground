116a82a9ad89c3f9d2014a688092d06b159f8c7b
Navaneeth Mohan <navaneethkrishnan24@gmail.com>
2025-12-19 09:51:14 +0530

    add simulator creation

--- remote_simulator/quantum-job-operator/internal/controller/quantumaerjob_controller.go
+++ remote_simulator/quantum-job-operator/internal/controller/quantumaerjob_controller.go
@@ -18,11 +18,19 @@ package controller
 
 import (
 	"context"
+	"fmt"
 	"time"
 
+	v1 "k8s.io/api/core/v1"
+	errors "k8s.io/apimachinery/pkg/api/errors"
+	"k8s.io/apimachinery/pkg/api/resource"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/types"
+
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/log"
 	logf "sigs.k8s.io/controller-runtime/pkg/log"
 
 	aerjobv2 "quantum/Aerjob/api/v2"
@@ -88,6 +96,91 @@ func (r *QuantumAerJobReconciler) Reconcile(ctx context.Context, req ctrl.Reques
 	}
 	return ctrl.Result{}, nil
 }
+
+func (r* QuantumAerJobReconciler) createSimulatorPod(ctx context.Context, job *aerjobv2.QuantumAerJob) (error){
+	
+	log := logf.FromContext(ctx)
+	podName := fmt.Sprintf("%s-sim-%d-%d", job.Name, job.Status.Retries, time.Now().Unix())
+
+	pod := &v1.Pod{
+		ObjectMeta: metav1.ObjectMeta{
+			Name : podName,
+			Namespace: job.Namespace,
+			Labels: map[string]string{
+				"app":  "quantum-simulator",
+				"quantum-job": job.Name,
+			},
+		},
+
+		Spec: v1.PodSpec{
+			RestartPolicy: v1.RestartPolicyNever,
+			ServiceAccountName: "quantum-simulator-sa",
+			Containers: []v1.Container{
+				{
+				Name : "aer-simulator",
+				Image : job.Spec.SimulatorImage,
+				Env : []v1.EnvVar{
+					{Name: "CIRCUITS", Value: job.Spec.Circuits},
+					{Name: "SHOTS", Value: fmt.Sprintf("%d", job.Spec.Shots)},
+					{Name: "BACKEND_NAME", Value: job.Spec.BackendName},
+					{Name: "JOB_ID", Value: job.Spec.JobID},
+					{Name: "QUANTUM_JOB_NAME", Value: job.Name},
+					{Name: "QUANTUM_JOB_NAMESPACE", Value: job.Namespace},
+					},
+				Resources: v1.ResourceRequirements{
+					Requests: v1.ResourceList{
+						v1.ResourceCPU: resource.MustParse("1"),
+						v1.ResourceMemory: resource.MustParse("512Mi"),
+						},
+					Limits: v1.ResourceList{
+						v1.ResourceCPU: resource.MustParse("2"),
+						v1.ResourceMemory: resource.MustParse("2Gi"),
+						},
+					},
+				},	
+			},
+		},
+	}
+
+
+	// set owner reference
+	if err := ctrl.SetControllerReference(job, pod, r.Scheme); err != nil{
+		return err
+	}
+
+	log.Info("Creating simulator pod", "podName", podName)
+
+	if err:= r.Create(ctx, pod); err != nil{
+		log.Info("Failed to create simulator pod", "podName", podName)
+		return err
+	}
+
+	job.Status.PodName = podName
+	return  r.Status().Update(ctx, job)
+}
+
+func (r* QuantumAerJobReconciler) getForPod(ctx context.Context, job *aerjobv2.QuantumAerJob)(pod *v1.Pod, err error){
+
+	log := logf.FromContext(ctx)
+	// fetches pod using name
+	if job.Status.PodName != "" {
+		var pod v1.Pod
+		err := r.Get(ctx, types.NamespacedName{
+			Name:      job.Status.PodName,
+			Namespace: job.Namespace,
+		}, &pod)
+
+		if err != nil{
+			log.Info("Failed to fetch simulator pod", "podName",job.Status.PodName)
+			return nil, err
+		}
+		log.Info("Sucessfully fetched the simulator pod", "podName",job.Status.PodName)
+		return &pod, err
+	}
+
+	return nil, errors.NewNotFound(v1.Resource("pod"), "")
+}
+	
 
 // SetupWithManager sets up the controller with the Manager.
 func (r *QuantumAerJobReconciler) SetupWithManager(mgr ctrl.Manager) error {
