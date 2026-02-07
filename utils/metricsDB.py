359c5026c62e2f2ddeedaa5d9717448167f3ca59
Navaneeth Mohan <navaneethkrishnan24@gmail.com>
2026-02-07 07:08:18 +0530

    methods to update status of transpilation and simulation

--- utils/metricsDB.py
+++ utils/metricsDB.py
@@ -82,6 +82,71 @@ class MetricsDB:
                 'time' : datetime.now(tz=timezone.utc)
             }
             cur.execute(query,params)
+    
+    def update_transpile_complete(self, job_id, duration_ms, circuit_depth=None, one_q_gate_count = None, two_q_gate_count = None):
+        """ Mark transpilation Ended """
+
+        with self.get_cursor() as cur:
+            query = """
+            UPDATE quantum_jobs
+            SET status = 'cr_created', 
+                transpile_end_at = %(time)s,
+                transpilation_duration_ms = %(duration)s,
+                circuit_depth = COALESCE(%(depth)s, circuit_depth),
+                one_q_gate_count = COALESCE(%(q1)s, one_q_gate_count),
+                two_q_gate_count = COALESCE(%(q2)s, two_q_gate_count),
+            WHERE job_id = %(job_id)s
+            """
+        
+            params = {
+                'job_id' : job_id,
+                'time' : datetime.now(tz=timezone.utc),
+                'duration': duration_ms,
+                'depth' : circuit_depth,
+                'q1' : one_q_gate_count,
+                'q2' : two_q_gate_count
+
+            }
+
+            cur.execute(query, params)
+
+    def update_simulation_start(self, job_id):
+        """ Mark Start of Simulation """
+
+        with self.get_cursor() as cur:
+
+            query = """
+            UPDATE quantum_jobs
+            SET status = 'in progress',
+                qpu_start_at = %(time)s,
+            WHERE job_id = %(job_id)s
+            """
+
+            params = {
+                'job_id' : job_id,
+                'time' : datetime.now(tz=timezone.utc)
+            }
+
+            cur.execute(query, params)
+    
+    def update_job_complete(self, job_id):
+
+        with self.get_cursor() as cur:
+
+            query = """
+            UPDATE quantum_jobs
+            SET status = 'completed',
+                completed_at = %(time)s,
+                total_duration_ms = EXTRACT(EPOCH FROM (%(time)s - submitted_at)) * 1000
+            WHERE job_id = %(job_id)s
+            """
+            params = {
+                'job_id' : job_id,
+                'time' : datetime.now(tz=timezone.utc)
+            }
+            cur.execute(query,params)
+
+    
 
 if __name__ == "__main__":
 
